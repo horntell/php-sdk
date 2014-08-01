48149b73b854a268b07650c55293dd7bb4df6aab
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-01 16:02:08 +0530

    made the App class

---
+++ src/Horntell/App.php
@@ -0,0 +1,85 @@
+<?php namespace Horntell\App;
+
+abstract class App {
+
+	/**
+	 * The app's key to authenticate the API requests
+	 * 
+	 * @var string
+	 */
+	public static $key;
+
+	/**
+	 * The app's secret to authenticate the API requests
+	 * 
+	 * @var string
+	 */
+	public static $secret;
+
+	/**
+	 * The base URL for Horntell API
+	 * 
+	 * @var string
+	 */
+	public static $base = 'http://api.horntell.com';
+
+	/**
+	 * The version of API to use
+	 * 
+	 * @var string
+	 */
+	public static $version = 'v1';
+
+	/**
+	 * Boots the app using key and secret
+	 * 
+	 * @param  string $key
+	 * @param  string $secret
+	 * @return void
+	 */
+	public function init($key, $secret)
+	{
+		self::$key = $key;
+		self::$secret = $secret;
+	}
+
+	/**
+	 * Returns the app's key
+	 * 
+	 * @return string
+	 */
+	public function getKey()
+	{
+		return self::$key;
+	}
+
+	/**
+	 * Returns the app's secret
+	 * 
+	 * @return string
+	 */
+	public function getSecret()
+	{
+		return self::$secret;
+	}
+
+	/**
+	 * Returns the base URL for API
+	 * 
+	 * @return string
+	 */
+	public function getBase()
+	{
+		return self::$base;
+	}
+
+	/**
+	 * Returns the API version to use
+	 * 
+	 * @return string
+	 */
+	public function getVersion()
+	{
+		return self::$version;
+	}
+}
