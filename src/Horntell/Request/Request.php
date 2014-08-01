15e3ad8c4ad49938b2bbea9bedc5d18c5f53bfbb
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-01 17:00:18 +0530

    made basic requester

---
+++ src/Horntell/Request/Request.php
@@ -0,0 +1,26 @@
+<?php namespace Horntell\Request;
+
+use Horntell\App;
+use GuzzleHttp\Client as Guzzle;
+
+class Request {
+
+	protected $client;
+
+	public function __construct()
+	{
+		$this->client = new Guzzle([
+			'base_url' => App::$base,
+			'defaults' => [
+				'auth' => [App::$key, App::$secret],
+				'headers' => ['Accept' => 'application/vnd.horntell.' . App::$version . '+json']
+			]
+		]);
+	}
+
+	public function send($method, $endpoint, $data = [])
+	{
+		$request = $this->client->createRequest($method, $endpoint, $data);
+		return $this->client->send($request);
+	}
+}
