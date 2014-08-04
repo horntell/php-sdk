0d0328e48181ea85f7279bc963abd0dde02d9e0f
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-04 16:17:23 +0530

    changed the type

--- src/Horntell/Errors/NetworkError.php
+++ src/Horntell/Errors/NetworkError.php
@@ -4,6 +4,6 @@ class NetworkError extends Error {
 	public function __construct()
 	{
 		$this->message = "Could not connect to Horntell. Please check your network connection and try again. If the problem persists, please get in touch with us at hello@horntell.com.";
-		$this->type = 'network_issue';
+		$this->type = 'network_error';
 	}
 }
