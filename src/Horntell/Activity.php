24f0deb9a93313be7dc35c75f6a2aea298ccb814
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-05 11:35:38 +0530

    docblocked activity class

--- src/Horntell/Activity.php
+++ src/Horntell/Activity.php
@@ -2,13 +2,28 @@
 
 class Activity {
 
+	/**
+	 * Instance of Request class
+	 *
+	 * @var Horntell\Http\Request
+	 */
 	protected $request;
 
+	/**
+	 * Constructor
+	 */
 	public function __construct()
 	{
 		$this->request = new Http\Request;
 	}
 
+	/**
+	 * Creates a new activity for a profile
+	 *
+	 * @param  string $uid
+	 * @param  array $activity
+	 * @return Horntell\Http\Response
+	 */
 	public function create($uid, $activity)
 	{
 		return $this->request->send('POST', "/profiles/$uid/activities", $activity);
