2d3c18ce29dfe8fdb19c7da53ab829fc07e5f51f
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-05 11:31:18 +0530

    docblocked response class

--- src/Horntell/Http/Response.php
+++ src/Horntell/Http/Response.php
@@ -2,16 +2,36 @@
 
 class Response {
 
+	/**
+	 * Instance of original response from HTTP client
+	 *
+	 * @var GuzzleHttp\Message\Response
+	 */
 	protected $response;
 
+	/**
+	 * Parsed JSON body
+	 *
+	 * @var array
+	 */
 	protected $body;
 
+	/**
+	 * Consructor
+	 *
+	 * @param GuzzleHttp\Message\Response $response
+	 */
 	public function __construct($response)
 	{
 		$this->response = $response;
 		$this->body = $this->response->json();
 	}
 
+	/**
+	 * Gets the parsed JSON body
+	 *
+	 * @return array|null (Null in case of No Content)
+	 */
 	public function getBody()
 	{
 		return $this->body;
