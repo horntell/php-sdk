284f25bdd49c28f3950504e8a898db7ccd1d22a2
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-05 11:09:08 +0530

    doc block added in network error

--- src/Horntell/Errors/NetworkError.php
+++ src/Horntell/Errors/NetworkError.php
@@ -1,6 +1,10 @@
 <?php namespace Horntell\Errors;
 
 class NetworkError extends Error {
+
+	/**
+	 * Constructor
+	 */
 	public function __construct()
 	{
 		$this->message = "Could not connect to Horntell. Please check your network connection and try again. If the problem persists, please get in touch with us at hello@horntell.com.";
