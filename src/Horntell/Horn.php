8b393dd5bc44504545d203d370a1620c89c592a0
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-05 11:38:39 +0530

    docblocked horn class

--- src/Horntell/Horn.php
+++ src/Horntell/Horn.php
@@ -2,13 +2,28 @@
 
 class Horn {
 
+	/**
+	 * Instance of Request class
+	 *
+	 * @var Horntell\Http\Request
+	 */
 	protected $request;
 
+	/**
+	 * Constructor
+	 */
 	public function __construct()
 	{
 		$this->request = new Http\Request;
 	}
 
+	/**
+	 * Sends a horn to a profile
+	 *
+	 * @param  string $uid
+	 * @param  array $horn
+	 * @return Horntell\Http\Response
+	 */
 	public function toProfile($uid, $horn)
 	{
 		return $this->request->send('POST', "/profiles/$uid/horns", $horn);
