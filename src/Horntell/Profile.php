b0983f733b4f395f580c4496523f922021562883
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-08-05 11:34:25 +0530

    docblocked profile class

--- src/Horntell/Profile.php
+++ src/Horntell/Profile.php
@@ -2,28 +2,61 @@
 
 class Profile {
 
+	/**
+	 * Instance of Request class
+	 *
+	 * @var Horntell\Http\Request
+	 */
 	protected $request;
 
+	/**
+	 * Constructor
+	 */
 	public function __construct()
 	{
 		$this->request = new Http\Request;
 	}
 
+	/**
+	 * Finds a profile by its uid
+	 *
+	 * @param  string $uid
+	 * @return Horntell\Http\Response
+	 */
 	public function find($uid)
 	{
 		return $this->request->send('GET', "/profiles/$uid");
 	}
 
+	/**
+	 * Creates a new profile
+	 *
+	 * @param  array $profile
+	 * @return Horntell\Http\Response
+	 */
 	public function create($profile)
 	{
 		return $this->request->send('POST', '/profiles', $profile);
 	}
 
+	/**
+	 * Updates a profile using its uid
+	 *
+	 * @param  string $uid
+	 * @param  array $profile
+	 * @return Horntell\Http\Response
+	 */
 	public function update($uid, $profile)
 	{
 		return $this->request->send('PUT', "/profiles/$uid", $profile);
 	}
 
+	/**
+	 * Deletes a profile using its uid
+	 *
+	 * @param  string $uid
+	 * @return Horntell\Http\Response
+	 */
 	public function delete($uid)
 	{
 		return $this->request->send('DELETE', "/profiles/$uid");
