cd93ac3a195c849d5ca186db87c28afa27aed832
Sahil <sahilsarpal@gmail.com>
2016-04-19 14:51:38 +0530

    added event method in php sdk to decode event payload

---
+++ src/Horntell/Event.php
@@ -0,0 +1,13 @@
+<?php namespace Horntell;
+
+class Event {
+
+	/**
+	 * decodes payload from input
+	 * @return Decoded Payload From Webhook
+	 */
+	public function fromWebhook()
+	{
+		return  $_POST['horntell_event'];
+	}
+}
